b5a83191969dabc2e3cb18e636a9b9a6d3337eac
monkey-bug <136386334+monkey-bug@users.noreply.github.com>
2025-02-08 15:30:28 -0600

    gamblers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { buildAuthorization } from "@retroachievements/api";
+import { buildAuthorization, getAchievementUnlocks } from "@retroachievements/api";
 import { compress, decompress } from "./compression.js";
 import { getOption, setOption } from "./options.js";
 import * as api from "./api.js";
@@ -13,6 +13,14 @@ function html(literals, ...expr) {
     }
 
     return string;
+}
+
+function toast(text) {
+    const div = document.createElement("div");
+    div.id = "toast";
+    div.innerText = text;
+    document.body.appendChild(div);
+    setTimeout(() => div.remove(), 3000);
 }
 
 async function getAuthorization() {
@@ -60,46 +68,93 @@ document.getElementById("verify").addEventListener("click", async () => {
 
     api.resetBackoff();
 
-    const users = Array.from(document.getElementById("users").value.split("\n"));
-    const untracked = [];
-
     switchToTab("output");
+    const output = document.getElementById("output")
     output.innerHTML = html`
-        <h1>Untracked</h1><hr />
-        <div class="progress">
-            <span class="spinner"></span>
-            <span class="amount"></span>
-        </div>
+        <h1>Gamblers</h1><hr />
+        <span class="spinner"></span>
     `;
 
-    const progress = output.querySelector(".progress");
-    const amount = output.querySelector(".progress > .amount");
+    const spinner = output.querySelector(".spinner");
+    let gamblers = [];
 
-    let i = 0;
-    for (const user of users) {
-        const result = await api.checkUser(auth, user);
-        if (result.untracked) {
-            untracked.push(result);
-        }
+    const achievementElem = document.getElementById("achievements");
+    const achievements = Array.from(achievementElem.querySelectorAll("input[type='text']"))
+        .map(input => input.value)
+        .map(value => Array.from(value.matchAll("https://(?:www.)?retroachievements.org/achievement/([0-9]+)")))
+        .flatMap(x => x.map(([_, id]) => id));
+
+    for (const achievement of achievements) {
+        const data = await getAchievementUnlocks(auth, {
+            achievementId: achievement,
+            count: 500,
+            offset: 0
+        });
         await api.wait();
-        amount.innerText = `${++i} / ${users.length}`;
+        console.log(data);
+        let n = data.unlocksCount - 500;
+        const unlocks = data.unlocks;
+        while (n > 0) {
+            const data = await getAchievementUnlocks(auth, {
+                achievementId: achievement,
+                count: 500,
+                offset: unlocks.length,
+            });
+            console.log(data);
+            await api.wait();
+            unlocks.push(...data.unlocks);
+            n -= 500;
+        }
+
+        if (gamblers.length == 0) {
+            gamblers.push(...unlocks.map(u => u.user));
+        } else {
+            gamblers = gamblers.filter(user => unlocks.map(u => u.user).includes(user));
+        }
+        console.log(gamblers);
     }
 
-    if (untracked.length == 0) {
-        progress.innerHTML = `No untracked users!`;
-        return;
-    }
-
-    for (const user of untracked) {
+    for (const user of gamblers) {
         const div = document.createElement("div");
         div.className = "user";
         div.innerHTML = html`
-            <img src=${`https://retroachievements.org${user.userPic}`}></img>
-            <a href=${`https://retroachievements.org/user/${user.username}`}>${user.username}</span>
+            <a href=${`https://retroachievements.org/user/${user}`}>${user}</a>
+            <span class="copy">📋</span>
         `;
-        progress.parentElement.append(div);
+
+        div.querySelector("span.copy").addEventListener("click", () => {
+            navigator.clipboard.writeText(user);
+            toast("Username copied to clipboard");
+        });
+        spinner.parentElement.append(div);
     }
-    progress.remove();
+    spinner.remove();
+
+    // let i = 0;
+    // for (const user of users) {
+    //     const result = await api.checkUser(auth, user);
+    //     if (result.untracked) {
+    //         untracked.push(result);
+    //     }
+    //     await api.wait();
+    //     amount.innerText = `${++i} / ${users.length}`;
+    // }
+    //
+    // if (untracked.length == 0) {
+    //     progress.innerHTML = `No untracked users!`;
+    //     return;
+    // }
+    //
+    // for (const user of untracked) {
+    //     const div = document.createElement("div");
+    //     div.className = "user";
+    //     div.innerHTML = html`
+    //         <img src=${`https://retroachievements.org${user.userPic}`}></img>
+    //         <a href=${`https://retroachievements.org/user/${user.username}`}>${user.username}</span>
+    //     `;
+    //     progress.parentElement.append(div);
+    // }
+    // progress.remove();
 });
 
 document.getElementById("clear").addEventListener("click", () => {
